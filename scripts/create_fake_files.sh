81fb04d6947de9faf1d387a9f0293a1e5d699a73
Cal Courtney <calthejuggler@gmail.com>
2024-12-22 10:25:32 +0000

    feat(test): add fake files bash script

---
+++ scripts/create_fake_files.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Usage: ./create_mkv_files.sh <count>
+# Example: ./create_mkv_files.sh 5
+# This will create:
+#   EXAMPLE_MOVIE_t00.mkv
+#   EXAMPLE_MOVIE_t01.mkv
+#   EXAMPLE_MOVIE_t02.mkv
+#   EXAMPLE_MOVIE_t03.mkv
+#   EXAMPLE_MOVIE_t04.mkv
+#   EXAMPLE_MOVIE_t05.mkv
+#
+
+# If the user didn't provide the count, default to 5
+COUNT=${1:-5}
+PREFIX="SHAUN_OF_THE_DEAD_t"
+
+for (( i=0; i<=COUNT; i++ )); do
+  # Use printf to pad the number with zero if needed
+  FILENAME=$(printf "%s%02d.mkv" "$PREFIX" "$i")
+  echo "Creating file: $FILENAME"
+  touch "$FILENAME"
+done
+
