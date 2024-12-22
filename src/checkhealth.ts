9800fdbf1ff00e634362353ae121c10e1ab9b22a
Cal Courtney <calthejuggler@gmail.com>
2024-12-22 14:22:16 +0000

    feat: example .env file

--- src/checkhealth.ts
+++ src/checkhealth.ts
@@ -5,4 +5,7 @@ export const checkHealth = () => {
     if (!envvars[key as keyof typeof envvars])
       throw new Error(`No value provided for ${key}`);
   }
+
+  if (envvars.INPUT_DIR === envvars.OUTPUT_DIR)
+    throw new Error("INPUT_DIR and OUTPUT_DIR cannot be the same");
 };
