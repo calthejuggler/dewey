835f9b8edad922260698f4c1d7df9de7f1978c91
Cal Courtney <calthejuggler@gmail.com>
2024-12-30 18:54:53 +0000

    chore: ran knip

--- src/envvars.ts
+++ src/envvars.ts
@@ -1,9 +1,7 @@
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const INPUT_DIR = process.env.INPUT_DIR;
 export const OUTPUT_DIR = process.env.OUTPUT_DIR;
 
 export default {
-  OPENAI_API_KEY,
-  INPUT_DIR,
-  OUTPUT_DIR,
+	INPUT_DIR,
+	OUTPUT_DIR,
 };
