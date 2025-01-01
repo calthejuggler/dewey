f04659f667c5277806e6650294aa8ba3bf2f9f46
Cal Courtney <calthejuggler@gmail.com>
2025-01-01 12:29:28 +0000

    feat: add STALE_TIME_MS envvar

--- src/envvars.ts
+++ src/envvars.ts
@@ -1,7 +1,12 @@
-export const INPUT_DIR = process.env.INPUT_DIR;
-export const OUTPUT_DIR = process.env.OUTPUT_DIR;
+export const INPUT_DIR = process.env.INPUT_DIR ?? "/input";
+export const OUTPUT_DIR = process.env.OUTPUT_DIR ?? "/output";
+
+export const STALE_TIME_MS = process.env.STALE_TIME_MS
+	? Number(process.env.STALE_TIME_MS)
+	: 1000 * 30;
 
 export default {
 	INPUT_DIR,
 	OUTPUT_DIR,
+	STALE_TIME_MS,
 };
