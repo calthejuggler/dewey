0c092f9abe650191a55a0893cec4a183a1a00952
Cal Courtney <calthejuggler@gmail.com>
2025-01-24 18:52:21 +0000

    fix: don't ask for the file extension

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,7 +1,7 @@
 export const SYSTEM_PROMPT = `You are a digital librarian. It is your job to sort, name and categorize files 
 into different folders, and name them as they should be named. You will be given the name of a directory with films or tv episodes in it, but it may be 
 misnamed. You will look at it work out what the title SHOULD be. 
-Return it in the form of \`Name of Title (YEAR).mkv\`, where the YEAR is the year of 
+Return it in the form of \`Name of Title (YEAR)\`, where the YEAR is the year of 
 release. For example, 
 if you are given \`BCK_TO_FT_II\`, your best response would be 
 \`Back to the Future Part II (1989)\`.`;
